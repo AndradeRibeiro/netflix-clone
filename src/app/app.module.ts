c1e260b40f01662fca11a93c8b7344d99723fa38
Ester Andrade Ribeiro da Silva <ester.silva@programmers.com.br>
2022-01-08 16:09:21 -0300

    Criando o projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FullBannerComponent } from './components/full-banner/full-banner.component';
+import { ListTitlesComponent } from './components/list-titles/list-titles.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TitleItemComponent } from './components/title-item/title-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FullBannerComponent,
+    ListTitlesComponent,
+    FooterComponent,
+    TitleItemComponent
   ],
   imports: [
     BrowserModule,
