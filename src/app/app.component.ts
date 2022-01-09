1ce48969990d856c553786bc7c01dd32ffc7ebcf
Ester Andrade Ribeiro da Silva <ester.silva@programmers.com.br>
2022-01-09 16:28:25 -0300

    Adicionando melhorias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,44 +11,104 @@ export class AppComponent {
 
   listTitles = [
     {
-      trailer: 'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
-      fullVideo: 'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
-      cover: 'assets/images/guardios-da-galaxia.jpg',
-      name: 'Guardiões da Galaxia',
+      trailer: 'assets/videos/fugindo_do_amor_trailer.mp4',
+      fullVideo: 'assets/videos/fugindo_do_amor_trailer.mp4',
+      cover: 'assets/images/fugindo_do_amor.jpg',
+      name: 'Fugindo do Amor',
       relevance: 98,
-      age: 14,
-      parts: 2,
-      categories: ['Épico', 'Filme de Fantasia', 'Viagem no Espaço']
+      age: 12,
+      parts: 0,
+      categories: ['Comédias românticas', 'Comédia', 'Filmes românticos']
     },
     {
-      trailer: 'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
-      fullVideo: 'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
-      cover: 'assets/images/guardios-da-galaxia.jpg',
-      name: 'Guardiões da Galaxia',
-      relevance: 98,
+      trailer: 'assets/videos/a_barraca_do_beijo_3_trailer.mp4',
+      fullVideo: 'assets/videos/a_barraca_do_beijo_3_trailer.mp4',
+      cover: 'assets/images/a_barraca_do_beijo_3.jpg',
+      name: 'A Barraca do beijo 3',
+      relevance: 97,
       age: 14,
-      parts: 2,
-      categories: ['Épico', 'Filme de Fantasia', 'Viagem no Espaço']
+      parts: 0,
+      categories: ['Comédias românticas', 'Comédia', 'Filmes românticos']
     },
     {
-      trailer: 'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
-      fullVideo: 'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
-      cover: 'assets/images/guardios-da-galaxia.jpg',
-      name: 'Guardiões da Galaxia',
+      trailer: 'assets/videos/fugindo_do_amor_trailer.mp4',
+      fullVideo: 'assets/videos/fugindo_do_amor_trailer.mp4',
+      cover: 'assets/images/fugindo_do_amor.jpg',
+      name: 'Fugindo do Amor',
       relevance: 98,
-      age: 14,
-      parts: 2,
-      categories: ['Épico', 'Filme de Fantasia', 'Viagem no Espaço']
+      age: 12,
+      parts: 0,
+      categories: ['Comédias românticas', 'Comédia', 'Filmes românticos']
     },
     {
-      trailer: 'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
-      fullVideo: 'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
-      cover: 'assets/images/guardios-da-galaxia.jpg',
-      name: 'Guardiões da Galaxia',
-      relevance: 98,
+      trailer: 'assets/videos/a_barraca_do_beijo_3_trailer.mp4',
+      fullVideo: 'assets/videos/a_barraca_do_beijo_3_trailer.mp4',
+      cover: 'assets/images/a_barraca_do_beijo_3.jpg',
+      name: 'A Barraca do beijo 3',
+      relevance: 97,
       age: 14,
-      parts: 2,
-      categories: ['Épico', 'Filme de Fantasia', 'Viagem no Espaço']
+      parts: 0,
+      categories: ['Comédias românticas', 'Comédia', 'Filmes românticos']
+    },
+    {
+      trailer: 'assets/videos/fugindo_do_amor_trailer.mp4',
+      fullVideo: 'assets/videos/fugindo_do_amor_trailer.mp4',
+      cover: 'assets/images/fugindo_do_amor.jpg',
+      name: 'Fugindo do Amor',
+      relevance: 98,
+      age: 12,
+      parts: 0,
+      categories: ['Comédias românticas', 'Comédia', 'Filmes românticos']
+    },
+    {
+      trailer: 'assets/videos/a_barraca_do_beijo_3_trailer.mp4',
+      fullVideo: 'assets/videos/a_barraca_do_beijo_3_trailer.mp4',
+      cover: 'assets/images/a_barraca_do_beijo_3.jpg',
+      name: 'A Barraca do beijo 3',
+      relevance: 97,
+      age: 14,
+      parts: 0,
+      categories: ['Comédias românticas', 'Comédia', 'Filmes românticos']
+    },
+    {
+      trailer: 'assets/videos/fugindo_do_amor_trailer.mp4',
+      fullVideo: 'assets/videos/fugindo_do_amor_trailer.mp4',
+      cover: 'assets/images/fugindo_do_amor.jpg',
+      name: 'Fugindo do Amor',
+      relevance: 98,
+      age: 12,
+      parts: 0,
+      categories: ['Comédias românticas', 'Comédia', 'Filmes românticos']
+    },
+    {
+      trailer: 'assets/videos/a_barraca_do_beijo_3_trailer.mp4',
+      fullVideo: 'assets/videos/a_barraca_do_beijo_3_trailer.mp4',
+      cover: 'assets/images/a_barraca_do_beijo_3.jpg',
+      name: 'A Barraca do beijo 3',
+      relevance: 97,
+      age: 14,
+      parts: 0,
+      categories: ['Comédias românticas', 'Comédia', 'Filmes românticos']
+    },
+    {
+      trailer: 'assets/videos/fugindo_do_amor_trailer.mp4',
+      fullVideo: 'assets/videos/fugindo_do_amor_trailer.mp4',
+      cover: 'assets/images/fugindo_do_amor.jpg',
+      name: 'Fugindo do Amor',
+      relevance: 98,
+      age: 12,
+      parts: 0,
+      categories: ['Comédias românticas', 'Comédia', 'Filmes românticos']
+    },
+    {
+      trailer: 'assets/videos/a_barraca_do_beijo_3_trailer.mp4',
+      fullVideo: 'assets/videos/a_barraca_do_beijo_3_trailer.mp4',
+      cover: 'assets/images/a_barraca_do_beijo_3.jpg',
+      name: 'A Barraca do beijo 3',
+      relevance: 97,
+      age: 14,
+      parts: 0,
+      categories: ['Comédias românticas', 'Comédia', 'Filmes românticos']
     }
   ];
 
